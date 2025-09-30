0d2d1cea7da3965145dda31a06c4ce40ece94fb4
Ayesha Abid <abidayesha428@gmail.com>
2025-09-30 16:21:21 +0500

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -65,28 +65,9 @@ public class Triangle{
     public String toString(){
         return( sideA, sideB, sideC); 
     }
-
-
-
-    //main.java
-    public static void main(String[] args) {
-        
-        Triangle t = new Triangle();
-
-        //setter
-        t.setSideA(3);
-        t.setSideB(4);
-        t.setSideC(6);
-
-        //getter
-        System.out.println("TRIANGLE");
-        System.out.println("SideA=" +t.getSideA());
-        System.out.println("SideB=" +t.getSideB());
-        System.out.println("SideC=" +t.getSideC());
-
-        System.out.println("Perimeter of Triangle=" +t.perimeter());
-        
-
-    }
 }
+
+
+    
+
 
